73a98eb7c0aafb414e901b0fe5d34ae2d6ca0be6
Sahidur <83564435+srmxpro@users.noreply.github.com>
2022-09-18 20:57:37 +0530

    Add files via upload

---
+++ Sum Two Numbers.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a,b,c=0;
+	
+	printf("Enter first no : ");
+	scanf("%d",&a);
+	printf("Enter second no : ");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf("The sum is %d",c);
+	
+	return 0;
+}
